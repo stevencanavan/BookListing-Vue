85208e836f793efc21ad822a953bb70b2f1d5a57
Steve Canavan <stevenrosscanavan@gmail.com>
2020-10-12 16:13:59 -0400

    updated template to render parent component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Books />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Books from './components/Books.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Books,
   },
 };
 </script>
